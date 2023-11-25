faae7e19f740a7b9a5a1a61f90047f4f74c97fd8
ramonaflores <angaritayelaj@gmail.com>
2023-11-25 12:33:48 -0500

    raul mamabicho kagul raul mamabecha gufi pisaje bidal gofrodedo dodofredo alex mesquita es gorfi cuidao

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.animationg3 {
 
 
     opens com.example.animationg3 to javafx.fxml;
+    exports com.example.animationg3.util;
     exports com.example.animationg3;
     exports com.example.animationg3.control;
     opens com.example.animationg3.control to javafx.fxml;
